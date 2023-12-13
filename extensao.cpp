489128b40ed6bee0d0ec555d33bcd933d8908e67
Thiago <heraclitothiago@hotmail.com>
2023-12-13 00:10:48 -0300

    estruturas criadas para referência de código

--- extensao.cpp
+++ extensao.cpp
@@ -1,8 +1,43 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-
-int main(int argc, char const *argv[])
+typedef struct address
 {
-    /* code */
+    char rua[50];
+    int numero;
+    char bairro[50];
+    char cidade[50];
+    char estado[2];
+    char cep[8];
+    char complemento[50];
+} Endereco;
+
+typedef struct datetime
+{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct vitima
+{
+    char nome[50];
+    char cpf[11];
+    int idade;
+    Endereco enderecoResidencial;
+    struct node *next;
+} Vitima;
+
+// estrutura fila - controlador
+typedef struct queue
+{
+    Vitima *inicio;
+    Vitima *fim;
+    int qtd;
+} Queue;
+
+int main()
+{
+
     return 0;
 }
