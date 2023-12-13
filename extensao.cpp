48d4027151d24a94466006f749a61334c991ffd5
Thiago <heraclitothiago@hotmail.com>
2023-12-13 00:21:36 -0300

    métodos adicionados

--- extensao.cpp
+++ extensao.cpp
@@ -29,12 +29,46 @@ typedef struct vitima
 } Vitima;
 
 // controlador de lista, evitar ponteiro para ponteiro
-typedef struct queue
+typedef struct lista
 {
     Vitima *inicio;
     Vitima *fim;
     int qtd;
-} Queue;
+} Lista;
+
+void startList(Lista *lista)
+{
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd = 0;
+}
+
+void criarRegistro(Lista *lista)
+{
+    Vitima *novo = (Vitima *)malloc(sizeof(Vitima));
+    // prompts
+    printf("Digite o nome da vitima: ");
+    scanf("%s", novo->nome);
+    printf("Digite seu cpf: ");
+    scanf("%s", novo->cpf);
+    printf("Digite sua idade: ");
+    scanf("%d", &novo->idade);
+    printf("Digite sua rua: ");
+    scanf("%s", novo->enderecoResidencial.rua);
+    printf("Digite o numero: ");
+    scanf("%d", &novo->enderecoResidencial.numero);
+    printf("Digite o bairro: ");
+    scanf("%s", novo->enderecoResidencial.bairro);
+    printf("Digite a cidade: ");
+    scanf("%s", novo->enderecoResidencial.cidade);
+    printf("Digite o estado: ");
+    scanf("%s", novo->enderecoResidencial.estado);
+    printf("Digite o cep: ");
+    scanf("%s", novo->enderecoResidencial.cep);
+    printf("Digite o complemento do endereço: ");
+    scanf("%s", novo->enderecoResidencial.complemento);
+
+}
 
 int main()
 {
